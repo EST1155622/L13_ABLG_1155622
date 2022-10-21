f43e60f054f70e5a5e8dcbedc4323bd8d837d0f5
estudiante <estudiante@T204-11>
2022-10-21 13:03:25 -0600

    Agregar archivos de proyecto.

---
+++ L13_ABLG_1155622/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace L13_ABLG_1155622
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] EDAD = new int[5];
+            string[] NOMBRES = new string[5];
+
+            Console.WriteLine("Ingresa el nombre de 5 personas");
+
+            for (int i=0; i<5; ++i)
+            {
+               
+               
+                Console.WriteLine("Ingresa el nombre: ");
+                NOMBRES[i] = Console.ReadLine();
+                Console.WriteLine("Ingresa la edad: "); 
+               
+                EDAD[i] = int.Parse(Console.ReadLine());
+
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                if (EDAD[i]>18)
+                {
+                    Console.WriteLine("El nombre es: " + NOMBRES[i]);
+                }
+            }
+
+        }
+    }
+}
